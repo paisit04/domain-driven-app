e9ece29ed0297c50ba358431e2b9bef86f9926ea
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2021-09-08 20:03:41 -0700

    Ch1 - update

--- app/Console/Commands/TestCustomer.php
+++ app/Console/Commands/TestCustomer.php
@@ -40,12 +40,14 @@ class TestCustomer extends Command
     public function handle()
     {
         $customer = new Customer;
+        $customer->name = "Jesse griffin";
         $customer->phone = "6197779125";
         $customer->phone_type = 1;
-        
+
+        $this->info('Name: ' . $customer->name);
         $this->info('Phone: ' . $customer->phone);
         $this->info('Phone Type: ' . $customer->phone_type);
-        $this->info('Raw Phone Type: ' . $customer->getAttribute('phone_type'));
+        $this->info('Raw Phone Type: ' . $customer->getAttributes()['phone_type']);
         return 0;
     }
 }
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,41 +2,41 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Customer extends Model
 {
     use HasFactory;
 
     public $table = 'customers';
-	protected $fillable = ['name', 'phone', 'phone_type'];
+    protected $fillable = ['name', 'phone', 'phone_type'];
 
     /**
-	 * Accessor returning the name with capital first letter
-	 */
-	public function getNameAttribute($name)
-	{
-		return ucfirst($name);
-	}
-	
-	/**
-	 * Accessor returning english version of phone_type
-	 */
-	public function getPhoneTypeAttribute($phone_type)
-	{
-		switch ($phone_type) {
-			case 1:
-				return "home phone";
-				break;
-			case 2:
-				return "cell phone";
-				break;
-			case 3: 
-				return "work phone";
-				break;
-			default:
-				throw new Exception("shit!");
-		}
-	}
+     * Accessor returning the name with capital first letter
+     */
+    public function getNameAttribute($name)
+    {
+        return ucfirst($name);
+    }
+
+    /**
+     * Accessor returning english version of phone_type
+     */
+    public function getPhoneTypeAttribute($phone_type)
+    {
+        switch ($phone_type) {
+            case 1:
+                return "home phone";
+                break;
+            case 2:
+                return "cell phone";
+                break;
+            case 3:
+                return "work phone";
+                break;
+            default:
+                throw new Exception("shit!");
+        }
+    }
 }
